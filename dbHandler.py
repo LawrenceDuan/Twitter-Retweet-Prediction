9f969eeaef5ca1bcbdc8a85482d179c04cdd1f3e
Cheng Duan <lawrence.chengduan@gmail.com>
2018-06-14 20:36:03 +0100

    Comment update.

--- dbHandler.py
+++ dbHandler.py
@@ -2,6 +2,10 @@ import pymongo
 
 
 def connectDB():
+    '''
+    Connect to mongodb database
+    :return: selected database db and the client connection
+    '''
     connection = pymongo.MongoClient('mongodb://127.0.0.1:27017')
     db = connection['tweets']
 
@@ -9,4 +13,9 @@ def connectDB():
 
 
 def closeDB(client):
+    '''
+    Disconnect to mongodb database
+    :param client: the client need to be disconnected
+    :return: void
+    '''
     client.close()
