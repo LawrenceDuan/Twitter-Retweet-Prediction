d7130848cb11ad7d4c21ee1601e7da27c2a4dba9
Cheng Duan <lawrence.chengduan@gmail.com>
2018-06-14 17:37:14 +0100

    Update.

--- tweetScoring.py
+++ tweetScoring.py
@@ -8,28 +8,28 @@ def scoring(db):
     twitter_users = list(db.retweetPrediction.find().distinct("username"))
 
     for user in twitter_users:
-        user_tweets = list(db.retweetPrediction.find({"username":user}).sort('date'))
+        user_tweets = list(db.retweetPrediction.find({"username":user}).sort([("date",1)]))
 
-        if "score" in tweets[0]:
-            print ("Skipping over " + user)
+        if "score" in user_tweets[0]:
+            print (user + "'s tweets has been scored before.")
         else:
-            if len(tweets) < 50:
+            if len(user_tweets) < 100:
                 db.retweetPrediction.remove({"username": user})
-                print ("Removed " + user + " from database")
+                print (user + "was removed from database as the number of related tweets is less than 100")
                 break
 
             time_retweets_tuples = []
-            for tweet in tweets:
+            for tweet in user_tweets:
                 tweet['timeFloat'] = functions.time_to_float(datetime.datetime.strptime(tweet['date'], '%Y-%m-%d %H:%M'))
                 time_retweets_tuples.append((tweet['timeFloat'], tweet['retweets']))
 
             m, b = functions.theilsen(time_retweets_tuples)
 
             i = 0
-            for tweet in tweets:
+            for tweet in user_tweets:
                 tweet['timeFloat'] = functions.time_to_float(datetime.datetime.strptime(tweet['date'], '%Y-%m-%d %H:%M'))
                 if i > 0:
-                    tweet['timeSinceLastTweet'] = tweet['timeFloat'] - tweets[i - 1]['timeFloat']
+                    tweet['timeSinceLastTweet'] = tweet['timeFloat'] - user_tweets[i - 1]['timeFloat']
                 i += 1
                 tweet['normal'] = m * tweet['timeFloat'] + b
                 try:
