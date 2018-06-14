c2af48d5b1d3ffa25c16cb286b529e9266442f65
Cheng Duan <lawrence.chengduan@gmail.com>
2018-06-14 22:54:07 +0100

    Important algorithm optimisation.

--- htmlHandler.py
+++ htmlHandler.py
@@ -1,15 +1,15 @@
-import urllib.request
-import urllib.parse
-import urllib.error
-import urllib.request
-import urllib.error
-import urllib.parse
 import json
 import re
 import datetime
 import sys
 import http.cookiejar
 from pyquery import PyQuery
+import urllib.request
+import urllib.parse
+import urllib.error
+import urllib.request
+import urllib.error
+import urllib.parse
 
 
 class HtmlHandler:
@@ -18,9 +18,43 @@ class HtmlHandler:
         pass
 
     @staticmethod
-    def getJsonReponse(userName, refreshCursor, cookieJar, proxy):
+    def writeToCSV(tweets, csv):
+        '''
+        Writing tweet information into the csv file
+        :param tweets: the extracted tweets
+        :param csv: the csv file to be written in
+        :return: void
+        '''
+        for tweet in tweets:
+            csv.write(('\n%s;%s;"%s";%d;"%s";%d;%d;%s;%s;%s;%s' % (tweet.username,
+                                                                tweet.date.strftime("%Y-%m-%d %H:%M"),
+                                                                tweet.tweetid,
+                                                                tweet.authorid,
+                                                                tweet.text,
+                                                                tweet.retweets,
+                                                                tweet.favorites,
+                                                                tweet.mentions,
+                                                                tweet.hashtags,
+                                                                tweet.permalink,
+                                                                tweet.geo)))
+        csv.flush()
+        print('\n Summary: ', end='')
+        print(len(tweets), end='')
+        print(" tweets crawled and saved.")
+
+
+    @staticmethod
+    def getJsonReponse(userName, e_cursor, cookieJar, proxy):
+        '''
+        Pretend to be a human who is reading through twitter.com. Then extract back html page information into json.
+        :param userName: name of the twitter account
+        :param e_cursor: current cursor position on web page
+        :param cookieJar: reading cookie on twitter.com
+        :param proxy: web proxy
+        :return: json of current twitter page
+        '''
         urlGetData = ' from:' + userName
-        url = "https://twitter.com/i/search/timeline?f=tweets&q=" + urllib.parse.quote(urlGetData) + "&src=typd&" + '' + "max_position=" + refreshCursor
+        url = "https://twitter.com/i/search/timeline?f=tweets&q=" + urllib.parse.quote(urlGetData) + "&src=typd&" + '' + "max_position=" + e_cursor
 
         headers = [
             ('Host', "twitter.com"),
@@ -47,82 +81,66 @@ class HtmlHandler:
         return jsonTweet
 
     @staticmethod
-    def getTweets(userName, receiveBuffer=None, bufferLength=100, proxy=None):
-        refreshCursor = ''
+    def getTweets(userName, csv, proxy=None):
+        '''
+        Get tweet information from twitter.com
+        :param userName: the name of twitter account
+        :param csv: the file to written in
+        :param proxy: proxy of web
+        :return: void
+        '''
+        e_cursor = ''
+        e_cursor_previous = 'none'
 
-        results = []
-        resultsAux = []
+        extractedFormattedTweetInfo = []
         cookieJar = http.cookiejar.CookieJar()
 
-        active = True
-
-        while active:
-            jsonTweet = HtmlHandler.getJsonReponse(userName, refreshCursor, cookieJar, proxy)
+        while e_cursor != e_cursor_previous :
+            # Pretend to be a human reading a html page and extract current page back in json
+            jsonTweet = HtmlHandler.getJsonReponse(userName, e_cursor, cookieJar, proxy)
             if len(jsonTweet['items_html'].strip()) == 0:
                 break
 
-            refreshCursor = jsonTweet['min_position']
+            # Control the cursor on the html
+            e_cursor_previous = e_cursor
+            e_cursor = jsonTweet['min_position']
             tweets = PyQuery(jsonTweet['items_html'])
-            # Remove incomplete tweets withheld by Twitter Guidelines
+
             tweets.remove('div.withheld-tweet')
             tweets = tweets('div.js-stream-tweet')
 
             if len(tweets) == 0:
                 break
 
-            for tweetHTML in tweets:
-                tweetPQ = PyQuery(tweetHTML)
+            for tweetPiece in tweets:
+                tweetPQ = PyQuery(tweetPiece)
                 tweet = Tweet()
 
-                usernameTweet = tweetPQ("span.username.js-action-profile-name b").text()
-                txt = re.sub(r"\s+", " ", tweetPQ("p.js-tweet-text").text().replace('# ', '#').replace('@ ', '@'))
-                retweets = int(tweetPQ("span.ProfileTweet-action--retweet span.ProfileTweet-actionCount").attr(
-                    "data-tweet-stat-count").replace(",", ""))
-                favorites = int(tweetPQ("span.ProfileTweet-action--favorite span.ProfileTweet-actionCount").attr(
-                    "data-tweet-stat-count").replace(",", ""))
-                dateSec = int(tweetPQ("small.time span.js-short-timestamp").attr("data-time"))
-                id = tweetPQ.attr("data-tweet-id")
-                permalink = tweetPQ.attr("data-permalink-path")
-                user_id = int(tweetPQ("a.js-user-profile-link").attr("data-user-id"))
-                geo = ''
-                geoSpan = tweetPQ('span.Tweet-geo')
-                if len(geoSpan) > 0:
-                    geo = geoSpan.attr('title')
-                urls = []
-                for link in tweetPQ("a"):
-                    try:
-                        urls.append((link.attrib["data-expanded-url"]))
-                    except KeyError:
-                        pass
-
-                tweet.id = id
-                tweet.permalink = 'https://twitter.com' + permalink
-                tweet.username = usernameTweet
-                tweet.text = txt
-                tweet.date = datetime.datetime.fromtimestamp(dateSec)
-                tweet.formatted_date = datetime.datetime.fromtimestamp(dateSec).strftime("%a %b %d %X +0000 %Y")
-                tweet.retweets = retweets
-                tweet.favorites = favorites
+                # Filter correspoding information from html
+                tweet.username = userName
+                tweet.date = datetime.datetime.fromtimestamp(int(tweetPQ("small.time span.js-short-timestamp").attr("data-time")))
+                tweet.tweetid = tweetPQ.attr("data-tweet-id")
+                tweet.authorid = int(tweetPQ("a.js-user-profile-link").attr("data-user-id"))
+                tweet.text = re.sub(r"\s+", " ", tweetPQ("p.js-tweet-text").text().replace('# ', '#').replace('@ ', '@'))
+                tweet.retweets = int(tweetPQ("span.ProfileTweet-action--retweet span.ProfileTweet-actionCount").attr("data-tweet-stat-count").replace(",", ""))
+                tweet.favorites = int(tweetPQ("span.ProfileTweet-action--favorite span.ProfileTweet-actionCount").attr("data-tweet-stat-count").replace(",", ""))
                 tweet.mentions = " ".join(re.compile('(@\\w*)').findall(tweet.text))
                 tweet.hashtags = " ".join(re.compile('(#\\w*)').findall(tweet.text))
-                tweet.geo = geo
-                tweet.urls = ",".join(urls)
-                tweet.author_id = user_id
+                tweet.permalink = 'https://twitter.com' + tweetPQ.attr("data-permalink-path")
+                if len(tweetPQ('span.Tweet-geo')) > 0:
+                    tweet.geo = tweetPQ('span.Tweet-geo').attr('title')
+                else:
+                    tweet.geo = ''
 
-                results.append(tweet)
-                resultsAux.append(tweet)
+                extractedFormattedTweetInfo.append(tweet)
+                print(" Progress: ", end='')
+                print(len(extractedFormattedTweetInfo), end='')
+                print(" tweets extracted from html.", end='\r')
 
-                if receiveBuffer and len(resultsAux) >= bufferLength:
-                    receiveBuffer(resultsAux)
-                    resultsAux = []
-
-        if receiveBuffer and len(resultsAux) > 0:
-            receiveBuffer(resultsAux)
-
-        return results
+        # Write what extracted form html page into csv file
+        HtmlHandler.writeToCSV(extractedFormattedTweetInfo, csv)
 
 
 class Tweet:
-
     def __init__(self):
         pass
