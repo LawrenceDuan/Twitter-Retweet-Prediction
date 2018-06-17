121aa3bf17428262bb9249a3a9163bb269a95165
Cheng Duan <lawrence.chengduan@gmail.com>
2018-06-18 00:53:19 +0100

    Algorithm debugged and updated.

--- textScoring.py
+++ textScoring.py
@@ -21,22 +21,23 @@ def get_parser():
 
 
 def createWordDictionary(user, user_tweets):
-    scoreCollection, dictionary = {}
+    scoreCollection = {}
+    dictionary = {}
 
     for tweet in user_tweets:
-        for word in wordsin(tweet['text']):
-            if word in scoreCollection: scoreCollection[word].append(tweet['score'])
-            else: scoreCollection[word] = [tweet['score']]
+        if type(tweet['text']) is str:
+            for word in wordsin(tweet['text']):
+                if word in scoreCollection: scoreCollection[word].append(tweet['score'])
+                else: scoreCollection[word] = [tweet['score']]
 
     for word in scoreCollection:
-        dictionary[word] = word
         dictionary[word] = numpy.mean(scoreCollection[word])
 
-    print("•" + user + "'s dictionary created!")
+    # print("•" + user + "'s dictionary created!")
 
-    descending_sorted_dictionary = sorted(dictionary.items(), reverse=True, key=lambda elem: elem[1])
-
-    return descending_sorted_dictionary
+    # descending_sorted_dictionary = sorted(dictionary.items(), reverse=True, key=lambda elem: elem[1])
+    # print(descending_sorted_dictionary)
+    return dictionary
 
 
 def wordsin(tweet):
@@ -55,8 +56,10 @@ def tweetScoring(tweet, dictionary):
     scoreList = []
 
     for word in wordsin(tweet):
-        if word in dictionary: scoreList.append(dictionary[word])
-        else: scoreList.append(0)
+        if word in dictionary:
+            scoreList.append(dictionary[word])
+        else:
+            scoreList.append(0)
 
     if scoreList == []: score = 0
     else: score = numpy.mean(scoreList)
@@ -73,17 +76,21 @@ if __name__ == "__main__":
     db, connection = dbHandler.connectDB()
 
     # Get users's tweets for text scoring
-    users = list(db.retweetPrediction.find().distinct("username"))
-    for user in users:
-        user_tweets = list(db.retweetPrediction.find({"username": user}).sort([("date", 1)]))
+    # users = list(db.retweetPrediction.find().distinct("username"))
+    # for user in users:
+    user_tweets = list(db.retweetPrediction.find({"username": args.user}).sort([("date", 1)]))
 
-        # Create word dictionary for the user
-        descending_sorted_dictionary = createWordDictionary(user, user_tweets)
+    # Create word dictionary for the user
+    dictionary = createWordDictionary(args.user, user_tweets)
 
-        # Calculating a new tweet's score
-        score = tweetScoring(args.tweet, descending_sorted_dictionary)
+    # Calculating a new tweet's score
+    score = tweetScoring(args.tweet, dictionary)
 
-        print('Predicted tweet score: ' + score)
+    print('• User: ' + args.user)
+    print('• Tweet: ' + args.tweet)
+    print('• Predicted tweet score: ', end='')
+    print(score)
+    print('-----------------------------------------------------------------------------------------')
 
     # Disconnect to mongodb
     dbHandler.closeDB(connection)
