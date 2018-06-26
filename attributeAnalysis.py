5ca2a185cf0beadf6a647e2b8327bc106259a15b
Cheng Duan <lawrence.chengduan@gmail.com>
2018-06-26 16:46:06 -0400

    Applying support vector regression.

--- attributeAnalysis.py
+++ attributeAnalysis.py
@@ -22,18 +22,40 @@ def attrValue(tweet, attr):
         return tweet['favorites']
 
 
-def model(tweets, attr):
-    tuples = []
-    
+def SVR(user_tweets, attr):
+    x_train, y_train = trainingPrep(user_tweets, attr)
+
+    clf = sklearn.svm.SVR(C=1e3, gamma=5e-10, kernel='rbf')
+    y_pred = clf.fit(x_train, y_train).predict(x_train)
+
+
+
+
+def trainingPrep(user_tweets, attr):
+
+    attrValue_score_tuples = []
+
+    for tweet in user_tweets:
+        attrValue_score_tuples.append((attrValue(tweet, attr), tweet['score']))
+
+    attrValue_score_tuples = sorted(attrValue_score_tuples, reverse=False, key=lambda elem: elem[0])
+
+    x_train = []
+    y_train = []
+
+    for tuple in attrValue_score_tuples:
+        x_train.append([tuple[0]])
+        y_train.append(tuple[1])
+
+    return numpy.array(x_train), numpy.array(y_train)
 
 
 if __name__ == "__main__":
     db, connection = dbHandler.connectDB()
     user_tweets = list(db.myCollection.find())
 
-    for tweet in user_tweets:
-        test = attributeValue(tweet, attribute='tweetlength')
+    test = trainingPrep(user_tweets, 'retweets')
 
-        print(str(test)+'---'+tweet['text'])
+    print(test[0][1])
 
     dbHandler.closeDB(connection)
