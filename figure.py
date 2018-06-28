68ca2d22be3660107666d5bd5985b105cb7b6dc4
Cheng Duan <lawrence.chengduan@gmail.com>
2018-06-28 18:01:35 -0400

    Automatically update related figures.

--- figure.py
+++ figure.py
@@ -1,13 +1,16 @@
 from matplotlib import pyplot as plt
 
-def draw_figure(improvements):
+def draw_figure(improvements, a, b, c):
     plt.title("Retweet Prediction Performance")
     plt.xlabel("Improvement over baseline in percentage (%)")
     plt.ylabel("Number of users")
     plt.hist(improvements)
-    plt.figtext(0.64, 0.85, 'No. of users: 1', fontsize=8)
-    plt.figtext(0.64, 0.83, 'No. of folds: 10', fontsize=8)
-    plt.figtext(0.64, 0.81, 'Average improvements: 59%', fontsize=8)
+    plt.figtext(0.64, 0.85, 'No. of users: ', fontsize=8)
+    plt.figtext(0.76, 0.85, a, fontsize=8)
+    plt.figtext(0.64, 0.83, 'No. of folds: ', fontsize=8)
+    plt.figtext(0.76, 0.83, b, fontsize=8)
+    plt.figtext(0.64, 0.81, 'Average improvements: ', fontsize=8)
+    plt.figtext(0.70, 0.79, c, fontsize=8)
 
     plt.show()
 
