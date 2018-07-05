311dbbddf8e5110cbd843cdca5ea1c2e90a62dd4
Cheng Duan <lawrence.chengduan@gmail.com>
2018-07-05 17:01:20 -0400

    Data normalisation. Calculating precision, recall, f1.

--- navigator.py
+++ navigator.py
@@ -99,7 +99,9 @@ def cross_validation(noofcv, user, user_tweets):
     K = int(noofcv)
     base = numpy.random.permutation(user_tweets)
     folds = []
-    outputs = []
+    targets = []
+    predicts = []
+
     for i in range(K):
         folds.append([])
     foldSize = math.ceil(len(user_tweets) / K);
@@ -128,6 +130,9 @@ def cross_validation(noofcv, user, user_tweets):
             actual_score = tweet['score']
             pred_score = predictor(tweet, weighted_tweet_pre_clf)
 
+            targets.append(actual_score)
+            predicts.append(pred_score)
+
             baseline_ses.append(actual_score ** 2)
             pred_ses.append((actual_score - pred_score) ** 2)
 
@@ -136,11 +141,71 @@ def cross_validation(noofcv, user, user_tweets):
 
     improvement = abs(baseline_mse - pred_mse) / baseline_mse * 100
 
-    return improvement
+    return improvement, targets, predicts
 
 
-def evaluation():
-
+# def evaluation(targets, predicts):
+#     confusion_matrix = numpy.zeros([11,11])
+#
+#     normalized_targets = []
+#     normalized_predicts = []
+#     # Data normalisation
+#     for i in range(len(targets) - 1):
+#         normalized_target = int((targets[i] - min(targets)) / (max(targets) - min(targets)) * 100)
+#         if normalized_target >= 0 and normalized_target <= 5:
+#             normalized_targets.append(0)
+#         elif normalized_target > 5 and normalized_target <= 15:
+#             normalized_targets.append(10)
+#         elif normalized_target > 15 and normalized_target <= 25:
+#             normalized_targets.append(20)
+#         elif normalized_target > 25 and normalized_target <= 35:
+#             normalized_targets.append(30)
+#         elif normalized_target > 35 and normalized_target <= 45:
+#             normalized_targets.append(40)
+#         elif normalized_target > 45 and normalized_target <= 55:
+#             normalized_targets.append(50)
+#         elif normalized_target > 55 and normalized_target <= 65:
+#             normalized_targets.append(60)
+#         elif normalized_target > 65 and normalized_target <= 75:
+#             normalized_targets.append(70)
+#         elif normalized_target > 75 and normalized_target <= 85:
+#             normalized_targets.append(80)
+#         elif normalized_target > 85 and normalized_target <= 95:
+#             normalized_targets.append(90)
+#         elif normalized_target > 95 and normalized_target <= 100:
+#             normalized_targets.append(100)
+#
+#         normalized_predict = int((predicts[i] - min(predicts)) / (max(predicts) - min(predicts)) * 100)
+#         if normalized_predict >= 0 and normalized_predict <= 5:
+#             normalized_predicts.append(0)
+#         elif normalized_predict > 5 and normalized_predict <= 15:
+#             normalized_predicts.append(10)
+#         elif normalized_predict > 15 and normalized_predict <= 25:
+#             normalized_predicts.append(20)
+#         elif normalized_predict > 25 and normalized_predict <= 35:
+#             normalized_predicts.append(30)
+#         elif normalized_predict > 35 and normalized_predict <= 45:
+#             normalized_predicts.append(40)
+#         elif normalized_predict > 45 and normalized_predict <= 55:
+#             normalized_predicts.append(50)
+#         elif normalized_predict > 55 and normalized_predict <= 65:
+#             normalized_predicts.append(60)
+#         elif normalized_predict > 65 and normalized_predict <= 75:
+#             normalized_predicts.append(70)
+#         elif normalized_predict > 75 and normalized_predict <= 85:
+#             normalized_predicts.append(80)
+#         elif normalized_predict > 85 and normalized_predict <= 95:
+#             normalized_predicts.append(90)
+#         elif normalized_predict > 95 and normalized_predict <= 100:
+#             normalized_predicts.append(100)
+#
+#     # print(len(normalized_targets), len(normalized_predicts))
+#
+#     for i in range(len(targets) - 1):
+#         print(int(normalized_targets[i]/10), int(normalized_predicts[i]/10), i)
+#         confusion_matrix[int(normalized_targets[i]/10), int(normalized_predicts[i]/10)] = confusion_matrix[int(normalized_targets[i]/10), int(normalized_predicts[i]/10)] + 1
+#
+#     print(confusion_matrix)
 
 
 if __name__ == '__main__':
@@ -151,8 +216,8 @@ if __name__ == '__main__':
     # Connect to mongodb
     db, connection = dbHandler.connectDB()
 
-    users = list(db.retweetPrediction.find().distinct("username"))
-    # users = ["taylorswift13"]
+    # users = list(db.retweetPrediction.find().distinct("username"))
+    users = ["taylorswift13"]
     improvements = []
     for user in users:
         print("• Building classifiers and performing cross validation for user: " + user)
@@ -166,9 +231,11 @@ if __name__ == '__main__':
         # Evaluation
         # Process k-fold cross validation
         # Split dataset into K folds
-        improvement = cross_validation(args.noofcv, user, user_tweets)
+        improvement, targets, predicts = cross_validation(args.noofcv, user, user_tweets)
         improvements.append(improvement)
         print("  Processing of " + user + " finished")
+
+       # evaluation(targets, predicts)
 
     figure.draw_figure(improvements, len(users), args.noofcv, numpy.mean(improvements))
 
